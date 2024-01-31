dd7eb0989ed654dd345cc666e62203f0d6b7be72
benboydens <r0857165@student.vives.be>
2024-01-31 23:50:38 +0100

    test

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const rewardsRouter = require('./routes/rewards');
+// const rewardsRouter = require('./routes/rewards');
 
 const app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, '/../public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/rewards', rewardsRouter);
+// app.use('/rewards', rewardsRouter);
 app.use('/css', express.static(path.join(__dirname, '/../node_modules/bootstrap/dist/css')));
 
 
